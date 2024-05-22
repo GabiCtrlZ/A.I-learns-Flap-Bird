1b69f50ebd6b1df3dce24d3436647188c0fff3e9
gabi b <gab3009@gmail.com>
2024-05-22 14:46:41 +0300

    feat(): made game harder and added alive and high score counters

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 
 let pop = new Population(30, 3)
 let highScoreOfficial = 0
-const pipeDiffHeight = 648
+const pipeDiffHeight = 650
 console.log(pop)
 const allPipes = AllPipes()
 const gameController = GameController()
